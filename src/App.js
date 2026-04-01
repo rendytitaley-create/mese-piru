a5adeac773b56fe13f9e92ec084250885b8b9e87
rendytitaley-create <rendytitaley@gmail.com>
2026-04-02 00:38:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1782,7 +1782,7 @@ const exportPresensiToPDF = () => {
                       <div className="flex items-center gap-6 mb-12 italic text-center md:text-left">
                         <div className="bg-indigo-50 p-5 rounded-[2rem] text-indigo-600 shadow-inner"><Award size={40}/></div>
                         <div className="italic text-left">
-                            <h2 className="text-2xl font-black text-slate-900 uppercase tracking-tighter italic">Peer Review {voteWindow.period.toUpperCase()} {voteWindow.evalYear}</h2>
+                            <h2 className="text-2xl font-black text-slate-900 uppercase tracking-tighter italic">Nominasi Pegawai Prima {voteWindow.period.toUpperCase()} {voteWindow.evalYear}</h2>
                             <p className="text-slate-400 text-[10px] font-black uppercase tracking-widest mt-1 italic">Beri Nilai Objektif Rekan Kerja Anda (Aktif 1-7 {new Date().toLocaleString('default', { month: 'long' })})</p>
                         </div>
                       </div>
